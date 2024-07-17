1cfc5dffee5a90712ad45d499f99d6c92e30da7a
dhaipayannaha <brookemmcburneym@gmail.com>
2024-07-18 02:56:53 +0600

    This is ci view

---
+++ application/config/routes.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+$route['default_controller'] = 'welcome';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
+
+$route['about'] = 'welcome/demo';
+
+$route['home'] = 'PageController/index';
+
+// pass paramiter 
+
+$route['blog/(:any)'] = 'PageController/blog/$1';
+
+// $route['blog/(:num)'] = 'PageController/blog/$1';
---
+++ application/views/demoPage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3><?php echo $title; ?></h3>
+    <h1> big thing is comming</h1>
+    <br>
+    <h3><?= $body; ?></h3>
+</body>
+</html>
